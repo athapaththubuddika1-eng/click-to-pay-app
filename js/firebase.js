c0d809094c5fce04d10f608792b3e2e16375dc8d
athapaththubuddika1-eng <athapaththubuddika1@gmail.com>
2025-10-12 18:19:22 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,5 @@
 // js/firebase.js
+// Realtime DB + Auth setup (edit firebaseConfig to your project if needed)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
