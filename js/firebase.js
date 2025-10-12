7c3fa750fc86eea0a08e57950f93f1d6ff14d933
athapaththubuddika1-eng <athapaththubuddika1@gmail.com>
2025-10-12 17:12:15 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,21 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase, ref, set, get, update, child, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+// js/firebase.js
+// Firebase modular SDK (Firestore + Auth)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBmKgh8-ckZFy_9-VlvcHD_sNxejKeS3pA",
   authDomain: "adsclickpay-b4870.firebaseapp.com",
-  databaseURL: "https://adsclickpay-b4870-default-rtdb.firebaseio.com",
   projectId: "adsclickpay-b4870",
-  storageBucket: "adsclickpay-b4870.appspot.com",
+  storageBucket: "adsclickpay-b4870.firebasestorage.app",
   messagingSenderId: "315468866481",
   appId: "1:315468866481:web:3ab95661be3939b8d7d390",
   measurementId: "G-3ZZ9K97QWJ"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-export { ref, set, get, update, child, onValue };
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
