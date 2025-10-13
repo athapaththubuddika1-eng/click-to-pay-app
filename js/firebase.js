abc634a1cfe15b1e3369741508c014b1a65420b0
athapaththubuddika1-eng <athapaththubuddika1@gmail.com>
2025-10-13 18:27:21 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,15 +3,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
 
+// ================== REPLACE WITH YOUR FIREBASE CONFIG ==================
 const firebaseConfig = {
-  apiKey: "AIzaSyBmKgh8-ckZFy_9-VlvcHD_sNxejKeS3pA",
-  authDomain: "adsclickpay-b4870.firebaseapp.com",
-  databaseURL: "https://adsclickpay-b4870-default-rtdb.firebaseio.com",
-  projectId: "adsclickpay-b4870",
-  storageBucket: "adsclickpay-b4870.appspot.com",
-  messagingSenderId: "315468866481",
-  appId: "1:315468866481:web:3ab95661be3939b8d7d390"
+  apiKey: "PASTE_YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 };
+// =====================================================================
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
